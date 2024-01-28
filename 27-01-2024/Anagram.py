a89ced6159d213288ef1d757e7755508965a562f
Pavish23 <157295016+Pavish23@users.noreply.github.com>
2024-01-28 23:01:07 +0530

    Anagram.py

--- 27-01-2024/Anagram.py
+++ 27-01-2024/Anagram.py
@@ -1,1 +1,59 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def anagram(s):
+    # Write your code here
+    if len(s) % 2 != 0:
+        return -1
+
+    mid = len(s) // 2
+    s1 = s[:mid]
+    s2 = s[mid:]
+
+    
+    count_s1 = [0] * 26  
+    count_s2 = [0] * 26
+
+    for char in s1:
+        count_s1[ord(char) - ord('a')] += 1
+
+    for char in s2:
+        count_s2[ord(char) - ord('a')] += 1
+
+    changes = 0
+    
+    for i in range(26):
+        changes += abs(count_s1[i] - count_s2[i])
+
+    
+    return changes // 2
+
+   
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+  
